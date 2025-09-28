07050dc602f19c610ce4ab67bb16f338f78c8009
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2025-09-28 04:21:45 -0300

    sign up registering working

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import logger from '#config/logger.js';
+import { createUser } from '#services/auth.service.js';
+import { cookies } from '#utils/cookies.js';
 import { formatValidationError } from '#utils/format.js';
+import { jwttoken } from '#utils/jwt.js';
 import { signUpSchema } from '#validations/auth.validation.js';
 
 export async function signup(req, res, next) {
@@ -12,16 +15,26 @@ export async function signup(req, res, next) {
         details: formatValidationError(validationResult.error),
       });
     }
-    const { name, email, role } = validationResult.data;
+    const { name, email, password, role } = validationResult.data;
+
+    const user = await createUser({ name, email, password, role });
+
+    const token = jwttoken.sign({
+      id: user.id,
+      email: user.email,
+      role: user.role,
+    });
+
+    cookies.set(res, 'token', token);
 
     logger.info(`User registered seccessfully :${email}`);
     res.status(201).json({
       message: 'User registered',
       user: {
-        id: 1,
-        name,
-        email,
-        role,
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
       },
     });
   } catch (e) {
